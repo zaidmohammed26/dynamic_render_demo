a98b63cf991591298c27421303db52c98831baa5
Zaid Mohammed <zaid.md221@gmail.com>
2024-07-29 23:42:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+let h1Text = "Default H1 Text";
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index", { h1Text });
+});
+
+app.get("/admin", (req, res) => {
+  res.render("admin", { h1Text });
+});
+
+app.post("/admin", (req, res) => {
+  h1Text = req.body.h1Text;
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
